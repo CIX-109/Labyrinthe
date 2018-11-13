1dd08ab79fd3f638151f577235c041a848ae96eb
CIX-109 <luca.gastaldello1@gmail.com>
2018-11-13 09:56:59 +0100

    Update JS/Script.js

--- JS/Script.js
+++ JS/Script.js
@@ -1,14 +1,20 @@
-function Generation() {
+function Test() {   
+}
+
+
+
+
+window.onload = function Generation() {
     
     var xt = 10,  yt = 10; //largeur et hauteur labyrinthe
     
-    var xe = 5, ye = 1 //case d'entrée labyrinthe
+    var xe = 5, ye = 0 //case d'entrée labyrinthe
     
     var xs = 5, ys = 9; //case de sortie labyrinthe
     
-    var r = 1; //nombre de chemin
+    var r = 1; //demi-tour
     
-    //direction N,S,E,O
+    var D; //direction N,S,E,O
     var N = true, S = true, E = true, O = true;
     
     var i = 0; //position dans le labyrinthe
@@ -17,54 +23,66 @@ function Generation() {
     
     var xtab = new Array(); //création tableau x
     var ytab = new Array(); //création tableau y
-    
+        
     var Vx = false, Vy = false, Victory = false; //condition de victoire
     
     
     //remplissage première ligne (0) tableaux
-    xtab = [xe]; //,'5','5','5','4','4','3'];
-    ytab = [ye]; //,'1','2','3','3','2','2'];
+    xtab = [xe];
+    ytab = [ye];
     
     do { //répéter boucle tant que case actuelle != sortie
         
         //réinitialisation directions
+        N = false;
+        S = false;
+        E = false;
+        O = false;
+                
+        //éviter de sortir des limites du labyrinthe
+        if (ytab[i] == 0) { N = true; }
+        else if (ytab[i] == yt) { S = true; }
+        else if (xtab[i] == 0) { E = true; }
+        else if (xtab[i] == xt) { O = true; }
+        else {
         N = true;
         S = true;
         E = true;
-        O = true;
-                
-        //eviter limites labyrinthe
-        if (ytab[i] === (yt-1)) { N = false; }
-        if (ytab[i] === 1) { S = false; }
-        if (xtab[i] === (xt-1)) { E = false; }
-        if (xtab[i] === 1) { O = false; }
+        O = true;    
+        if (ytab[i] >= (yt-1)) { N = false; }
+        if (ytab[i] <= 1) { S = false; }
+        if (xtab[i] >= (xt-1)) { E = false; }
+        if (xtab[i] <= 1) { O = false; }
+        }
     
         for (j = 0; j <= i; j++) {
             
-            //eviter doublon    
+            //éviter le chemin de retourner à une ancienne position  
             if ((xtab[i] === xtab[j]) && ((ytab[i]+1) === ytab[j])) { N = false; }
             if ((xtab[i] === xtab[j]) && ((ytab[i]-1) === ytab[j])) { S = false; }
             if (((xtab[i]+1) === xtab[j]) && (ytab[i] === ytab[j])) { E = false; }
             if (((xtab[i]-1) === xtab[j]) && (ytab[i] === ytab[j])) { O = false; }
-            
- 
         }
         
         //réinitialisation si aucun chemin possible
-        if ((N === false) && (S === false) && (E === false) && (O === false)) {
-            xtab = [xe], ytab = [ye];
-            i = 0;
-            r++;
+        if (!N && !S && !E && !O) {
+            
+            ytab[i+1] = ytab[(i-r)];
+            xtab[i+1] = xtab[(i-r)]; 
+            r=r+2;
         }
+    
         
-        else  {
-        
-            do {
+        else  {//si un chemin possible ...
+            
+            r=1;//réinitialisation compteur "demi-tour"
+            
+            do { //randomise une direction jusqu'à ce qu'elle soit possible
             k = Math.floor((Math.random() * 4) + 1);
             
                 switch (k) {
                     case 1:
-                    if (N === true) {   
+                    if (N === true) {  
                     ytab[i+1] = ytab[i]+1;
                     xtab[i+1] = xtab[i]; }
                     else { k = 5; }
@@ -97,28 +115,65 @@ function Generation() {
                 
             } while (k==5);
         
-        i++; } //fin else
+        } //fin else
+        i++;
         
-        //i = xtab.length;
-        
-        Vx = true;
-        Vy = true;
-        
+        //vérification des conditions de victoire
         if ((xtab[i]) == xs) { Vx = true; }
         else { Vx = false; }
         if ((ytab[i]) == ys) { Vy = true; }
         else { Vy = false; }
-     
         if ((Vx) && (Vy)) { Victory = true; }        
         
     } while (!Victory);
     
-    document.write("Nombre de chemins crées: " + r + "<br/><br/>");
-    document.write("Nombre de positions créées: " + i + "<br/><br/>");
+    
+    //écriture liste des positions
     for (j = 0; j <= i; j++) {
         document.write("Position " + j + " : ");
         document.write(xtab[j] + " | ");
         document.write(ytab[j] + "<br/>");
     }
-  
-}
+    
+    //réinitialisation direction
+    N = false;
+    S = false;
+    E = false;
+    O = false;
+    
+    var x, y; //compteurs
+    var dtab = new Array(); //création tableau D
+    
+    for (y = 1; y < yt; y++) { //balayage de tous les y
+        for (x = 1; x < xt; x++) { //balayage de tous les x
+            for (j = 1; j <= i; j++) { //balayage de toutes les positions (i)
+                if (((xtab[j]) == x) && ((ytab[j]) == y)) {
+                
+                    if ((ytab[j-1]) == (ytab[j]+1)) { N = true; }
+                    if ((ytab[j-1]) == (ytab[j]-1)) { S = true; }
+                    if ((xtab[j-1]) == (xtab[j]+1)) { E = true; }
+                    if ((xtab[j-1]) == (xtab[j]-1)) { O = true; }
+                    if ((ytab[j+1]) == (ytab[j]+1)) { N = true; }
+                    if ((ytab[j+1]) == (ytab[j]-1)) { S = true; }
+                    if ((xtab[j+1]) == (xtab[j]+1)) { E = true; }
+                    if ((xtab[j+1]) == (xtab[j]-1)) { O = true; }            
+                    
+                } //fin if               
+            } //fin balayage i
+            dtab[j]="";
+            if (N) { dtab[j]="N"; }
+            if (S) { dtab[j]=dtab[j].concat("S"); }
+            if (E) { dtab[j]=dtab[j].concat("E"); }
+            if (O) { dtab[j]=dtab[j].concat("O"); }
+            document.write("(" + x + ";" + y + "): " + dtab[j] + "<br/>");
+            N = false;
+            S = false;
+            E = false;
+            O = false;
+        } //fin balayage x
+    } //fin balayage y*/
+        
+        
+    
+    
+} //fin fonction Generation
